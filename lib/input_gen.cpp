80745683215ab07ad4ad8096e1c4f170abb94987
Tom Vempala <54186859+tomthomasvempala@users.noreply.github.com>
2022-02-12 20:14:08 +0530

    input gen on progress

--- lib/input_gen.cpp
+++ lib/input_gen.cpp
@@ -1,20 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+ifstream MyReadFile("../params/ip_format.txt");;
+
+unordered_map<string, int> data;
+
+void integer(string &oneLine)
+{
+    int x = rand() % 20;
+    cout << x;
+    int space2 = oneLine.find(' ');
+    string word2 = oneLine.substr(0, space2);
+    oneLine = oneLine.substr(space2 + 1);
+    data[word2] = x;
+    if (oneLine == "nl")
+    {
+        cout << endl;
+    }
+    else
+    {
+        cout << " ";
+    }
+}
+
 int main()
 {
-    string myText;
-
-    // Read from the text file
-    ifstream MyReadFile("./params/ip_format.txt");
-
-    // Use a while loop together with the getline() function to read the file line by line
-    while (getline(MyReadFile, myText))
+    freopen("../params/gen_inputs/ip.txt", "w", stdout);
+    
+    string oneLine;
+    srand(time(0));
+    while (getline(MyReadFile, oneLine))
     {
-        // Output the text from the file
-        cout << myText;
+        int space = oneLine.find(' ');
+        if (space != -1)
+        {
+            string word = oneLine.substr(0, space);
+            oneLine = oneLine.substr(space + 1);
+            if (word == "int")
+            {
+                integer(oneLine);
+            }
+            else if (word == "for")
+            {
+                int c = oneLine[0];
+                // if (isalpha())
+            }
+        }
     }
-
-    // Close the file
     MyReadFile.close();
 }
